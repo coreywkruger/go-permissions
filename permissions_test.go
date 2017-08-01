ad27896e9c1b204576943784dd910e2aac397979
Corey Kruger <coreykruger@Coreys-Macbook-Pro.local>
2017-08-01 14:58:50 -0700

    added test setup/teardown

--- permissions_test.go
+++ permissions_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/jmoiron/sqlx"
 	"github.com/spf13/viper"
 	"log"
 	"os"
 	"testing"
 )
+
+var db *sqlx.DB
 
 func TestMain(m *testing.M) {
 	config := viper.New()
@@ -18,19 +21,40 @@ func TestMain(m *testing.M) {
 	var dbconfig DbConfig
 	config.UnmarshalKey("database", &dbconfig)
 
-	db, err := InitDB(dbconfig)
+	db, err = InitDB(dbconfig)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, _ = db.Exec(`
+	_, err = db.Exec(`
 		drop table if exists apps cascade;
 		drop table if exists permissions cascade;
 		drop table if exists role_permissions cascade;
 		drop table if exists roles cascade;
 		drop table if exists entity_roles cascade;
 	`)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	exitVal := m.Run()
 
 	os.Exit(exitVal)
+}
+
+func TestCreatePermission(t *testing.T) {
+	err := cleanup(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cleanup(database *sqlx.DB) error {
+	_, err := database.Exec(`
+		drop table if exists apps cascade;
+		drop table if exists permissions cascade;
+		drop table if exists role_permissions cascade;
+		drop table if exists roles cascade;
+		drop table if exists entity_roles cascade;
+	`)
+	return err
 }
